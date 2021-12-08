89b4e160550a9913f3a78e5434c73703fdcd6af7
Oleg Shitikov <shitikov.om@phystech.edu>
2021-12-09 01:39:37 +0300

    buffer for network

---
+++ buffer/priorityBuffer.py
@@ -0,0 +1,67 @@
+from sumTree import SumTree
+
+import numpy as np
+import random
+
+
+class ReplayBuffer(object):
+    def __init__(self, size, alpha, eps):
+        """Create Replay buffer.
+        Parameters
+        ----------
+        size: int
+            Max number of transitions to store in the buffer. When the buffer
+            overflows the old memories are dropped.
+        alpha: float
+            Determines how much prioritization is used. alpha==0 - uniform randomized buffer
+        """
+        self._storage = SumTree(size)
+        self._alpha = alpha  # alpha determines how much prioritization is used
+        self._eps = eps  # epsilon smooths priority, priority = (TD_error + eps) ** alpha
+
+    def __len__(self):
+        return len(self._storage)
+
+    def add(self, obs_t, action, reward, obs_tp1, done, priority):
+        data = (obs_t, action, reward, obs_tp1, done)
+        self._storage.insert(data, priority)
+
+    def _encode_sample(self, records):
+        ids, obses_t, actions, rewards, obses_tp1, dones = [], [], [], [], [], []
+        for data in records:
+            idx, (obs_t, action, reward, obs_tp1, done) = data
+            obses_t.append(np.array(obs_t, copy=False))
+            actions.append(np.array(action, copy=False))
+            rewards.append(reward)
+            obses_tp1.append(np.array(obs_tp1, copy=False))
+            dones.append(done)
+            ids.append(idx)
+        return np.array(obses_t), np.array(actions),\
+               np.array(rewards), np.array(obses_tp1),\
+               np.array(dones), np.array(ids)
+
+    def sample(self, batch_size):
+        """Sample a batch of experiences.
+        Parameters
+        ----------
+        batch_size: int
+            How many transitions to sample.
+        Returns
+        -------
+        obs_batch: np.array
+            batch of observations
+        act_batch: np.array
+            batch of actions executed given obs_batch
+        rew_batch: np.array
+            rewards received as results of executing act_batch
+        next_obs_batch: np.array
+            next set of observations seen after executing act_batch
+        done_mask: np.array
+            done_mask[i] = 1 if executing act_batch[i] resulted in
+            the end of an episode and 0 otherwise.
+        idx: np.array
+            indexes of observations
+        """
+        segment = self._storage.total_sum() / batch_size
+        records = [self._storage.get(np.random.uniform(i * segment, (i+1) * segment)) for i in range(batch_size)]
+        return self._encode_sample(records)
---
+++ buffer/sumTree.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+
+class SumTree:
+    def __init__(self, size):
+        self.is_full = False
+        self._size = 0
+        self._maxsize = size
+        self._next_idx = 0
+        self._tree = np.zeros(2 * size - 1)
+        self._data = np.zeros(size, dtype=object)
+
+    def __len__(self):
+        return self._size
+
+    def _propagate(self, idx, change):
+        parent = (idx - 1) // 2
+        self._tree[parent] += change
+        if parent != 0:
+            self._propagate(parent, change)
+
+    def _retrieve(self, idx, s):
+        left = 2 * idx + 1
+        right = left + 1
+
+        # current node is leaf node
+        if left >= len(self._tree):
+            return idx
+
+        if s <= self._tree[left]:
+            return self._retrieve(left, s)
+        else:
+            return self._retrieve(right, s - self._tree[left])
+
+    def total_sum(self):
+        return self._tree[0]
+
+    def insert(self, data, priority):
+        self._data[self._next_idx] = data
+
+        idx = self._next_idx + self._maxsize - 1
+        self.update(idx, priority)
+
+        self._next_idx += 1
+        if self._next_idx >= self._maxsize:
+            self._next_idx = 0
+            self._size = self._maxsize
+        if self._size != self._maxsize:
+            self._size += 1
+
+    def update(self, idx, priority):
+        change = priority - self._tree[idx]
+
+        self._tree[idx] = priority
+        self._propagate(idx, change)
+
+    def get(self, s):
+        idx = self._retrieve(0, s)
+        data_idx = idx - self._maxsize + 1
+
+        # returns tuple: (tree_idx, data)
+        return idx, self._data[data_idx]
